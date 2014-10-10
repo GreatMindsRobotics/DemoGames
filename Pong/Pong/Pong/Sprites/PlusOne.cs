612327a9cb58c253340bd6ddcc59046a1d711a2a
JessicaPlotkin <0906jessica@gmail.com>
2014-10-09 18:15:27 -0700

    Fixed sliding sprite for score

--- Pong/Pong/Pong/Sprites/PlusOne.cs
+++ Pong/Pong/Pong/Sprites/PlusOne.cs
@@ -32,10 +32,11 @@ namespace Pong.Sprites
             _scale *= 0.99f;
 
             //TODO: Change to if statements; clamping will not work due to wrap around values. DO NOT decrement if value reaches 0 for each color componenet
-            _tintColor = new Color(MathHelper.Clamp(--_tintColor.R, 0, 255), 
-                                    MathHelper.Clamp(--_tintColor.G, 0, 255), 
-                                    MathHelper.Clamp(--_tintColor.B, 0, 255), 
-                                    MathHelper.Clamp(--_tintColor.A, 0, 255));
+           
+            _tintColor = new Color(_tintColor.R = (_tintColor.R != 0) ? (byte)(_tintColor.R - 1) : _tintColor.R,
+                                    _tintColor.G = (_tintColor.G != 0) ? (byte)(_tintColor.G - 1) : _tintColor.G,
+                                    _tintColor.B = (_tintColor.B != 0) ? (byte)(_tintColor.B - 1) : _tintColor.B, 
+                                    _tintColor.A = (_tintColor.A != 0) ? (byte)(_tintColor.A - 1) : _tintColor.A);
 
             if (Position == SlideTo)
             {
