3d44562b3975e43898cad243bb6761d3f3a1dca3
JessicaPlotkin <0906jessica@gmail.com>
2014-10-09 19:48:46 -0700

    Missed one file from last commit
    GameScreen changes related to commit
    d6de35045fd4c97a616b1d94a6e8e58840c55f65

--- Pong/Pong/Pong/Screens/GameScreen.cs
+++ Pong/Pong/Pong/Screens/GameScreen.cs
@@ -6,6 +6,8 @@ using Pong.Sprites;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using FontEffectsLib.FontTypes;
+using Pong.CoreTypes;
 
 
 
@@ -18,9 +20,15 @@ namespace Pong.Screens
         Paddle rightPaddle;
         PlusOne plusOne;
 
+        FadingFont leftScoreFont;
+        FadingFont rightScoreFont;
+
         KeyboardState keyboard;
-        
+
         Random rnd = new Random();
+
+        bool leftSideScored = false;
+
 
         int ballDirection;
         int paddleSpeed = 8;
@@ -31,15 +39,58 @@ namespace Pong.Screens
         public override void Load(Microsoft.Xna.Framework.Content.ContentManager Content)
         {
             leftPaddle = new Paddle(Content.Load<Texture2D>("temp paddle"), new Vector2(0, 176), Color.White);
-            rightPaddle = new Paddle(Content.Load<Texture2D>("temp paddle"), new Vector2(_viewPort.Width - leftPaddle.Texture.Width,176), Color.White);
-            ball = new Ball(Content.Load<Texture2D>("temp ball"), new Vector2(_viewPort.Width / 2 , _viewPort.Height / 2), Color.White);
-            ball.Position = new Vector2(_viewPort.Width/2 - ball.Texture.Width/2, _viewPort.Height/2 - ball.Texture.Height/2);
-            plusOne = new PlusOne(Content.Load<Texture2D>("Plus1"), new Vector2(_viewPort.Width / 2, _viewPort.Height / 2), Color.Red);            
-            
+            rightPaddle = new Paddle(Content.Load<Texture2D>("temp paddle"), new Vector2(_viewPort.Width - leftPaddle.Texture.Width, 176), Color.White);
+            ball = new Ball(Content.Load<Texture2D>("temp ball"), new Vector2(_viewPort.Width / 2, _viewPort.Height / 2), Color.White);
+            ball.Position = new Vector2(_viewPort.Width / 2 - ball.Texture.Width / 2, _viewPort.Height / 2 - ball.Texture.Height / 2);
+
+            plusOne = new PlusOne(Content.Load<Texture2D>("Plus1"), new Vector2(_viewPort.Width / 2, _viewPort.Height / 2), Color.Red);
+            plusOne.SlideCompleted += new FontEffectsLib.SpriteTypes.SlidingSprite.SlideCompletedState(plusOne_SlideCompleted);
+
+            leftScoreFont = new FadingFont(Content.Load<SpriteFont>("SpriteFont1"), new Vector2(10, 0), 0.1f, 1.0f, 0.01f, 1.0f, leftScore.ToString(), Color.White, false);
+            leftScoreFont.EnableShadow = false;
+
+
+            rightScoreFont = new FadingFont(Content.Load<SpriteFont>("SpriteFont1"), new Vector2(_viewPort.Width - 30, 0), 0.1f, 1.0f, 0.01f, 1.0f, rightScore.ToString(), Color.White, false);
+            rightScoreFont.EnableShadow = false;
+
+
             _sprites.Add(rightPaddle);
             _sprites.Add(leftPaddle);
             _sprites.Add(ball);
             _sprites.Add(plusOne);
+            _sprites.Add(leftScoreFont);
+            _sprites.Add(rightScoreFont);
+        }
+
+        void plusOne_SlideCompleted()
+        {
+            if (leftSideScored)
+            {
+                leftScore += 1;
+                leftScoreFont.Text.Clear().Append(leftScore);
+                leftScoreFont.Fade = true;
+            }
+            else
+            {
+                rightScore += 1;
+                rightScoreFont.Text.Clear().Append(rightScore);
+                rightScoreFont.Fade = true;
+            }
+
+            //Check for win
+            if (leftScore >= 10)
+            {
+
+                ScreenManager.Change(ScreenState.GameOver);
+                
+            }
+            else if (rightScore >= 10)
+            {
+
+                ScreenManager.Change(ScreenState.GameOver);
+
+            }
+
         }
 
         public override void Update(GameTime gameTime)
@@ -51,13 +102,13 @@ namespace Pong.Screens
                 //System.Diagnostics.Debugger.Break();
             }
 
-            if(keyboard.IsKeyDown(Keys.Space))
+            if (keyboard.IsKeyDown(Keys.Space))
             {
-                if(ball.BallState == BallState.Rested)
+                if (ball.BallState == BallState.Rested)
                 {
                     ballDirection = rnd.Next(0, 2);
 
-                    switch(ballDirection)
+                    switch (ballDirection)
                     {
                         case 0:
                             ball.Speed = new Vector2(5, 2);
@@ -75,22 +126,22 @@ namespace Pong.Screens
                 ball.BallState = BallState.Moving;
             }
 
-            if(ball.BallState == BallState.Moving)
+            if (ball.BallState == BallState.Moving)
             {
                 if (ball.Position.Y <= 0 || ball.Position.Y + ball.Texture.Height >= _viewPort.Height)
                 {
                     ball.SpeedY *= -1;
 
                 }
-                else if (ball.Position.X <= 0 )
+                else if (ball.Position.X <= 0)
                 {
                     //Ball goes through the wall
                     //TODO Add scoring
 
-                    rightScore += 1;
+                    leftSideScored = false;
 
                     plusOne.Position = new Vector2(_viewPort.Width * 3 / 4 - plusOne.Texture.Width / 2, _viewPort.Height / 2 - plusOne.Texture.Height / 2);
-                    plusOne.SlideTo = new Vector2(_viewPort.Width, 0);
+                    plusOne.SlideTo = new Vector2(_viewPort.Width - 30, 0);
                     plusOne.IsVisible = true;
 
                     //Reset the ball
@@ -103,10 +154,10 @@ namespace Pong.Screens
                     //Ball goes through the wall
                     //TODO Add scoring
 
-                    leftScore += 1;
+                    leftSideScored = true;
 
-                    plusOne.Position = new Vector2(_viewPort.Width * 1/ 4 - plusOne.Texture.Width / 2, _viewPort.Height / 2 - plusOne.Texture.Height / 2);
-                    plusOne.SlideTo = new Vector2(0, 0);
+                    plusOne.Position = new Vector2(_viewPort.Width * 1 / 4 - plusOne.Texture.Width / 2, _viewPort.Height / 2 - plusOne.Texture.Height / 2);
+                    plusOne.SlideTo = new Vector2(20, 0);
                     plusOne.IsVisible = true;
 
 
@@ -119,7 +170,7 @@ namespace Pong.Screens
             }
 
             //Rightpaddle Movement
-            if (keyboard.IsKeyDown(Keys.Up) && rightPaddle.Position.Y > 0 )
+            if (keyboard.IsKeyDown(Keys.Up) && rightPaddle.Position.Y > 0)
             {
                 rightPaddle.VectorY -= paddleSpeed;
             }
@@ -130,18 +181,18 @@ namespace Pong.Screens
             }
 
             //Leftpaddle Movement
-            if(keyboard.IsKeyDown(Keys.W) && leftPaddle.Position.Y > 0)
+            if (keyboard.IsKeyDown(Keys.W) && leftPaddle.Position.Y > 0)
             {
                 leftPaddle.VectorY -= paddleSpeed;
             }
 
-            if(keyboard.IsKeyDown(Keys.S) && leftPaddle.Position.Y + leftPaddle.Texture.Height < _viewPort.Height)
+            if (keyboard.IsKeyDown(Keys.S) && leftPaddle.Position.Y + leftPaddle.Texture.Height < _viewPort.Height)
             {
                 leftPaddle.VectorY += paddleSpeed;
             }
 
             //Checking if ball hit rightPaddle
-            if(ball.Position.X < rightPaddle.Position.X + rightPaddle.Texture.Width && ball.Position.X + ball.Texture.Width > rightPaddle.Position.X && ball.Position.Y < rightPaddle.Position.Y + rightPaddle.Texture.Height && ball.Texture.Height +  ball.Position.Y > rightPaddle.Position.Y)
+            if (ball.Position.X < rightPaddle.Position.X + rightPaddle.Texture.Width && ball.Position.X + ball.Texture.Width > rightPaddle.Position.X && ball.Position.Y < rightPaddle.Position.Y + rightPaddle.Texture.Height && ball.Texture.Height + ball.Position.Y > rightPaddle.Position.Y)
             {
                 //ball intersected with rightPaddle!!! Is it traveling to the right? If so, inverse direction; otherwise, leave it alone
                 if (ball.SpeedX > 0)
