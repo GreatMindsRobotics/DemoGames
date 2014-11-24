5d91a640cd3d7d3134c551110b3b5f60d4b0e859
Alexander Leontiev <aleontiev00@gmail.com>
2014-11-23 16:10:35 -0800

    Setting up Debug class

---
+++ Pong/Pong/Pong/CoreTypes/Debugger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Pong.Sprites;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using FontEffectsLib.FontTypes;
+using Pong.CoreTypes;
+using System.Xml.Linq;
+using FontEffectsLib.SpriteTypes;
+
+namespace Pong.CoreTypes
+{
+    public static class Debugger
+    {
+        public static void BallLine(Ball ball, GameSprite line)
+        { 
+           //ball stuff
+           while(true)
+           {
+            
+           } 
+        }
+    }
+}
